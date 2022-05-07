a46fba5eb65051f41d8e298f73069339fb05267c
Achref <medachrefg@gmail.com>
2022-05-07 10:24:39 +0100

    adding and fixing some config issues &adding qr_code page

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -3,20 +3,19 @@ import 'package:qr_code/delayed_animation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:qr_code/main.dart';
 
-
+import './qr_code.dart';
 
 class WelcomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEDECF2),
       body: SingleChildScrollView(
         child: Container(
-        margin: EdgeInsets.symmetric(
-          vertical:60,
-          horizontal:30,
-        ),
+          margin: EdgeInsets.symmetric(
+            vertical: 60,
+            horizontal: 30,
+          ),
           child: Column(
             children: [
               DelayedAnimation(
@@ -27,54 +26,55 @@ class WelcomePage extends StatelessWidget {
                 ),
               ),
               DelayedAnimation(
-                  delay: 2500,
-                  child: Container(
-                    height: 400,
-                    child: Image.asset('images/sterilizer.png'),
-              ),
+                delay: 2500,
+                child: Container(
+                  height: 400,
+                  child: Image.asset('images/sterilizer.png'),
+                ),
               ),
               DelayedAnimation(
                 delay: 3500,
                 child: Container(
                   height: 400,
                   margin: const EdgeInsets.only(
-                    top:30,
-                    bottom:20,
+                    top: 30,
+                    bottom: 20,
                   ),
-                  child:  Text("c'est primordial, pour qu'on vivons dans un milieu clarifier",
+                  child: Text(
+                    "c'est primordial, pour qu'on vivons dans un milieu clarifier",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.poppins(
-                        color: Colors.grey,
-                        fontSize:16,
-                    ), 
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
               ),
-    DelayedAnimation(
-    delay: 4500,
-    child: Container(
-    width: double.infinity,
-    child: ElevatedButton(
-    style: ElevatedButton.styleFrom(
-    primary: d_red,
-    shape: StadiumBorder(),
-    padding: EdgeInsets.all(13)),
-    child: Text('Scan'),
-    onPressed: () {
-    Navigator.push(
-    context,
-    MaterialPageRoute(
-    builder: (context) => Text('******'),
-    ),
-    );
-    },
-    ),
-    ),
-    ),
+              DelayedAnimation(
+                delay: 4500,
+                child: Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: d_red,
+                        shape: StadiumBorder(),
+                        padding: EdgeInsets.all(13)),
+                    child: Text('Scan'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QRViewExample(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
+        ),
       ),
-    ),
     );
   }
 }
